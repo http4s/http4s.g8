51a693004a01b1bd53921e857b2547378dd47c62
Benjamin Neil <ben@neil-concepts.com>
2017-04-11 20:49:52 -0600

    Mod, adding in fs2 as a replacement for scalaz Task

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -4,10 +4,13 @@ version := "0.0.1-SNAPSHOT"
 scalaVersion := "$scala_version$"
 
 val Http4sVersion = "$http4s_version$"
+val FS2Version = "$fs2_version$"
 
 libraryDependencies ++= Seq(
  "org.http4s"     %% "http4s-blaze-server" % Http4sVersion,
  "org.http4s"     %% "http4s-circe"        % Http4sVersion,
  "org.http4s"     %% "http4s-dsl"          % Http4sVersion,
+ "co.fs2"         %% "fs2-core"            % FS2Version,
+ "co.fs2"         %% "fs2-io"              % FS2Version,
  "ch.qos.logback" %  "logback-classic"     % "1.2.1"
 )
--- src/main/g8/src/main/scala/$package__packaged$/Server.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Server.scala
@@ -4,22 +4,23 @@ import java.util.concurrent.{ExecutorService, Executors}
 
 import scala.util.Properties.envOrNone
 
-import scalaz.concurrent.Task
+import fs2.Task
 
-import org.http4s.server.{Server, ServerApp}
+import org.http4s.server.Server
+import org.http4s.util.StreamApp
 import org.http4s.server.blaze.BlazeBuilder
 
 
-object BlazeExample extends ServerApp {
+object BlazeExample extends StreamApp {
 
   val port : Int              = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
   val ip   : String           = "0.0.0.0"
   val pool : ExecutorService  = Executors.newCachedThreadPool()
 
-  override def server(args: List[String]): Task[Server] =
+  override def server(args: List[String]) =
     BlazeBuilder
       .bindHttp(port, ip)
       .mountService(HelloWorld.service)
       .withServiceExecutor(pool)
-      .start
+      .serve
 }
