c0c9a21922b45047d26c6937190121bf1e4fe265
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2017-08-21 18:20:41 +0200

    Use ExecutionContext from pool

--- src/main/g8/src/main/scala/$package__packaged$/Server.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Server.scala
@@ -15,10 +15,12 @@ object BlazeExample extends StreamApp {
 
   val port : Int              = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
   val ip   : String           = "0.0.0.0"
+  val pool : ExecutorService  = Executors.newCachedThreadPool()
 
   override def stream(args: List[String]): Stream[Task, Nothing] =
     BlazeBuilder
       .bindHttp(port, ip)
       .mountService(HelloWorld.service)
+      .withExecutionContext(ExecutionContext.fromExecutorService(pool))
       .serve
 }
