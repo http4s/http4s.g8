cdad9b34ec2c79e2e01465d9fd5ebe09b9c304f8
Travis CI <travis-ci@http4s.org>
2017-10-05 15:40:13 -0400

    Fix what broke in 0.18.0-M2

--- src/main/g8/src/main/scala/$package__packaged$/HelloWorldServer.scala
+++ src/main/g8/src/main/scala/$package__packaged$/HelloWorldServer.scala
@@ -1,29 +1,23 @@
 package $package$
 
 import cats.effect.IO
-import org.http4s.server.blaze.BlazeBuilder
-import org.http4s.util.{StreamApp, _}
-
 import io.circe._
 import org.http4s._
 import org.http4s.circe._
+import org.http4s.dsl.Http4sDsl
 import org.http4s.server._
-import org.http4s.dsl._
+import org.http4s.server.blaze.BlazeBuilder
+import org.http4s.util.StreamApp
 
-import scala.util.Properties.envOrNone
-
-object BlazeExample extends StreamApp[IO] {
+object BlazeExample extends StreamApp[IO] with Http4sDsl[IO] {
   val route = HttpService[IO] {
     case GET -> Root / "hello" / name =>
       Ok(Json.obj("message" -> Json.fromString(s"Hello, \${name}")))
   }
 
   def stream(args: List[String], requestShutdown: IO[Unit]) = {
-    val port : Int              = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
-    val ip   : String           = "0.0.0.0"
-
     BlazeBuilder[IO]
-      .bindHttp(port, ip)
+      .bindHttp(8080, "0.0.0.0")
       .mountService(route,"/")
       .serve
   }
