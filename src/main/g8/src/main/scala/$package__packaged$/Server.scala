21d3627810b9c79710d8f044d2d025ed12d6360e
Benjamin Neil <dev.benjamin.neil@gmail.com>
2017-10-23 11:03:43 -0600

    Mod, Server, changing the server to use stream app

--- src/main/g8/src/main/scala/$package__packaged$/Server.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Server.scala
@@ -2,24 +2,21 @@ package $package$
 
 import java.util.concurrent.{ExecutorService, Executors}
 
+import fs2.Task
+
 import scala.util.Properties.envOrNone
-
-import scalaz.concurrent.Task
-
-import org.http4s.server.{Server, ServerApp}
 import org.http4s.server.blaze.BlazeBuilder
+import org.http4s.util.StreamApp
 
 
-object BlazeExample extends ServerApp {
+object Server extends StreamApp {
 
   val port : Int              = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
   val ip   : String           = "0.0.0.0"
   val pool : ExecutorService  = Executors.newCachedThreadPool()
 
-  override def server(args: List[String]): Task[Server] =
-    BlazeBuilder
-      .bindHttp(port, ip)
-      .mountService(HelloWorld.service)
-      .withServiceExecutor(pool)
-      .start
+  def stream(args: List[String]): fs2.Stream[Task, Nothing] = BlazeBuilder.bindHttp(8080)
+    .withWebSockets(true)
+    .mountService(HelloWorld.service, "/")
+    .serve
 }
