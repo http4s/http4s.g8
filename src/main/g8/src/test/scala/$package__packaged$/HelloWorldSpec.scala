af46a6562664f86d136c73431210d0a529e0819f
Benjamin Neil <dev.benjamin.neil@gmail.com>
2017-10-23 11:11:06 -0600

    Fix, tests, changing the specification around
    o changed 'in' to '>>'
    o cleaned up deps

--- src/main/g8/src/test/scala/$package__packaged$/HelloWorldSpec.scala
+++ src/main/g8/src/test/scala/$package__packaged$/HelloWorldSpec.scala
@@ -1,15 +1,14 @@
 package $package$
-
 import org.http4s.dsl._
 import org.http4s.{Method, Request, Response, Status}
-import org.specs2._
+import org.specs2.matcher.MatchResult
 
-class HelloWorldSpec extends Specification {
-  "HelloWorld" should {
-    "return 200" in {
+class HelloWorldSpec extends org.specs2.mutable.Specification {
+  "HelloWorld" >> {
+    "return 200" >> {
       uriReturns200()
     }
-    "return hellow world" in {
+    "return hellow world" >> {
       uriReturnsHelloWorld()
     }
   }
@@ -21,9 +20,8 @@ class HelloWorldSpec extends Specification {
   }
 
   private[this] def uriReturns200(): MatchResult[Status] =
-    retHelloWorld.status === Status.Ok
+    retHelloWorld.status must beEqualTo(Status.Ok)
 
   private[this] def uriReturnsHelloWorld(): MatchResult[String] =
-    retHelloWorld.as[String].unsafeRun() === "Hello, world"
+    retHelloWorld.as[String].unsafeRun() must beEqualTo("{\"message\":\"Hello, world\"}")
 }
-
