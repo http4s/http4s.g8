f72e50bc951f33a38963278a2e268acaccfbf22d
Benjamin Neil <dev.benjamin.neil@gmail.com>
2017-10-23 14:13:33 -0600

    Mod, server, removing unused executor

--- src/main/g8/src/main/scala/$package__packaged$/Server.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Server.scala
@@ -1,7 +1,5 @@
 package $package$
 
-import java.util.concurrent.{ExecutorService, Executors}
-
 import fs2.Task
 
 import scala.util.Properties.envOrNone
@@ -12,10 +10,8 @@ import org.http4s.util.StreamApp
 object Server extends StreamApp {
 
   val port : Int              = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
-  val ip   : String           = "0.0.0.0"
-  val pool : ExecutorService  = Executors.newCachedThreadPool()
 
-  def stream(args: List[String]): fs2.Stream[Task, Nothing] = BlazeBuilder.bindHttp(8080)
+  def stream(args: List[String]): fs2.Stream[Task, Nothing] = BlazeBuilder.bindHttp(port)
     .withWebSockets(true)
     .mountService(HelloWorld.service, "/")
     .serve
