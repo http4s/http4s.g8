9a0dd00099069a0205e69cc769d29d31bf79b4a4
Benjamin Neil <ben@neil-concepts.com>
2017-10-25 20:32:28 -0600

    Mod, formatting, removing an extra line and remove websockets since none are being used in the sample application

--- src/main/g8/src/main/scala/$package__packaged$/Server.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Server.scala
@@ -6,13 +6,11 @@ import scala.util.Properties.envOrNone
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.util.StreamApp
 
-
 object Server extends StreamApp {
 
   val port : Int              = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
 
   def stream(args: List[String]): fs2.Stream[Task, Nothing] = BlazeBuilder.bindHttp(port)
-    .withWebSockets(true)
     .mountService(HelloWorld.service, "/")
     .serve
 }
