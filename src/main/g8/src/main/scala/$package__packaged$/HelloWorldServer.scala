a2f89e9f63c44078c8a8ad8ed720852fa41c1692
JCranky <paulo.siqueira@zalando.de>
2018-03-20 14:50:17 +0100

    Extract the server stream definition

--- src/main/g8/src/main/scala/$package__packaged$/HelloWorldServer.scala
+++ src/main/g8/src/main/scala/$package__packaged$/HelloWorldServer.scala
@@ -3,13 +3,18 @@ package $package$
 import cats.effect.{Effect, IO}
 import fs2.StreamApp
 import org.http4s.server.blaze.BlazeBuilder
-import scala.concurrent.ExecutionContext.Implicits.global
 
-object HelloWorldServerApp extends HelloWorldServer[IO]
+import scala.concurrent.ExecutionContext
 
-abstract class HelloWorldServer[F[_]: Effect] extends StreamApp[F] {
+object HelloWorldServer extends StreamApp[IO] {
+  import scala.concurrent.ExecutionContext.Implicits.global
 
-  def stream(args: List[String], requestShutdown: F[Unit]) =
+  def stream(args: List[String], requestShutdown: IO[Unit]) = ServerStream.stream[IO]
+}
+
+object ServerStream {
+
+  def stream[F[_]: Effect](implicit ec: ExecutionContext) =
     BlazeBuilder[F]
       .bindHttp(8080, "0.0.0.0")
       .mountService(HelloWorldService.service, "/")
