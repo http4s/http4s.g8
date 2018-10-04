bb636f2c510f9014ee4cb013214ec114b873b558
Chris Davenport <chris@christopherdavenport.tech>
2018-10-04 15:40:13 -0400

    Use Client DSL

--- src/main/g8/src/main/scala/$package__packaged$/Jokes.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Jokes.scala
@@ -7,6 +7,8 @@ import io.circe.{Encoder, Decoder, Json, HCursor}
 import io.circe.generic.semiauto._
 import org.http4s.{EntityDecoder, EntityEncoder, Method, Uri, Request}
 import org.http4s.client.Client
+import org.http4s.client.dsl.Http4sClientDsl
+import org.http4s.Method._
 import org.http4s.circe._
 
 trait Jokes[F[_]]{
@@ -27,12 +29,10 @@ object Jokes {
   final case class JokeError(e: Throwable) extends RuntimeException
 
   def impl[F[_]: Sync](C: Client[F]): Jokes[F] = new Jokes[F]{
+    val dsl = new Http4sClientDsl[F]{}
+    import dsl._
     def get: F[Jokes.Joke] = {
-      val request = Request[F](
-        Method.GET,
-        Uri.uri("https://icanhazdadjoke.com/")
-      )
-      C.expect[Joke](request)
+      C.expect[Joke](GET(Uri.uri("https://icanhazdadjoke.com/")))
         .adaptError{ case t => JokeError(t)} // Prevent Client Json Decoding Failure Leaking
     }
   }
