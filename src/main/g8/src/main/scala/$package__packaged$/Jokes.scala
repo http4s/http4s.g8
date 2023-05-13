629a7095ab23e54d9b946e930aa0cef887d631d5
Channing Walton <channingwalton@mac.com>
2023-05-13 19:53:06 +0100

    Update http4s and syntax

--- src/main/g8/src/main/scala/$package__packaged$/Jokes.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Jokes.scala
@@ -1,14 +1,14 @@
 package $package$
 
 import cats.effect.Concurrent
-import cats.implicits._
+import cats.syntax.all.*
 import io.circe.{Encoder, Decoder}
-import org.http4s._
-import org.http4s.implicits._
+import org.http4s.*
+import org.http4s.implicits.*
 import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
-import org.http4s.circe._
-import org.http4s.Method._
+import org.http4s.circe.*
+import org.http4s.Method.*
 
 trait Jokes[F[_]]:
   def get: F[Jokes.Joke]
@@ -27,7 +27,7 @@ object Jokes:
 
   def impl[F[_]: Concurrent](C: Client[F]): Jokes[F] = new Jokes[F]:
     val dsl = new Http4sClientDsl[F]{}
-    import dsl._
+    import dsl.*
     def get: F[Jokes.Joke] = 
       C.expect[Joke](GET(uri"https://icanhazdadjoke.com/"))
         .adaptError{ case t => JokeError(t)} // Prevent Client Json Decoding Failure Leaking
